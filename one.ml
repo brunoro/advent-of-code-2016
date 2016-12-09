9f381d8085c1bc29de5d5ef39bf30a2e97869605
Gustavo Brunoro <git@hitnail.net>
2016-12-09 11:59:34 +0100

    first commit, beginning of first day

---
+++ one.ml
@@ -0,0 +1,16 @@
+#require "topfind"
+#require "batteries"
+
+let examples = [
+  ("R2, L3", 5);
+  ("R2, R2, R2", 2);
+  ("R5, L5, R5, R3", 12)
+]
+
+let input = "R5, R4, R2, L3, R1, R1, L4, L5, R3, L1, L1, R4, L2, R1, R4, R4, L2, L2, R4, L4, R1, R3, L3, L1, L2, R1, R5, L5, L1, L1, R3, R5, L1, R4, L5, R5, R1, L185, R4, L1, R51, R3, L2, R78, R1, L4, R188, R1, L5, R5, R2, R3, L5, R3, R4, L1, R2, R2, L4, L4, L5, R5, R4, L4, R2, L5, R2, L1, L4, R4, L4, R2, L3, L4, R2, L3, R3, R2, L2, L3, R4, R3, R1, L4, L2, L5, R4, R4, L1, R1, L5, L1, R3, R1, L2, R1, R1, R3, L4, L1, L3, R2, R4, R2, L2, R1, L5, R3, L3, R3, L1, R4, L3, L3, R4, L2, L1, L3, R2, R3, L2, L1, R4, L3, L5, L2, L4, R1, L4, L4, R3, R5, L4, L1, L1, R4, L2, R5, R1, R1, R2, R1, R5, L1, L3, L5, R2"
+
+let directions = "NESW"
+
+let parse_command str =
+  let open BatString in
+  (str.[0], str |> slice ~first:1 |> to_int)
