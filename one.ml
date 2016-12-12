17db97c2c603621108ccf259281994424a7020c3
gus <brunoro@devio.us>
2016-12-12 20:28:25 -0200

    reads input from files

--- one.ml
+++ one.ml
@@ -7,8 +7,6 @@ let examples = [
   ("R5, L5, R5, R3", 12)
 ]
 
-let input = "R5, R4, R2, L3, R1, R1, L4, L5, R3, L1, L1, R4, L2, R1, R4, R4, L2, L2, R4, L4, R1, R3, L3, L1, L2, R1, R5, L5, L1, L1, R3, R5, L1, R4, L5, R5, R1, L185, R4, L1, R51, R3, L2, R78, R1, L4, R188, R1, L5, R5, R2, R3, L5, R3, R4, L1, R2, R2, L4, L4, L5, R5, R4, L4, R2, L5, R2, L1, L4, R4, L4, R2, L3, L4, R2, L3, R3, R2, L2, L3, R4, R3, R1, L4, L2, L5, R4, R4, L1, R1, L5, L1, R3, R1, L2, R1, R1, R3, L4, L1, L3, R2, R4, R2, L2, R1, L5, R3, L3, R3, L1, R4, L3, L3, R4, L2, L1, L3, R2, R3, L2, L1, R4, L3, L5, L2, L4, R1, L4, L4, R3, R5, L4, L1, L1, R4, L2, R5, R1, R1, R2, R1, R5, L1, L3, L5, R2"
-
 let direction_labels = "NESW"
 
 let step_vectors dir = 
@@ -55,5 +53,12 @@ let run_test (str, expected) =
   let result = process_input str in
   Printf.printf "result: %d\t expected: %d\n" result expected
 
+let process_file filename = 
+  BatFile.lines_of filename
+  |> BatList.of_enum
+  |> List.hd
+  |> process_input
+
 let () = 
-  Printf.printf "%d\n" (process_input input)
+  ignore (List.iter run_test examples);
+  Printf.printf "result: %d\n" (process_file "one.txt")
